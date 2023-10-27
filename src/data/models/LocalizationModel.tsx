e4514d8e87ee1a030e6a3f2d0fef5dd66b507b6c
Dariusz Pluta <d.pluta@ideo.pl>
2023-10-27 11:46:05 +0200

    get current user location (init)

--- src/data/models/LocalizationModel.tsx
+++ src/data/models/LocalizationModel.tsx
@@ -1,5 +1,6 @@
 export class LocalizationModel {
     id!: number;
+    coord!: LatLang;
     name?: string;
     locality?: string;
     country?: string;
@@ -7,4 +8,9 @@ export class LocalizationModel {
     static city(localization: LocalizationModel): string {
         return localization.locality ?? localization.name ?? 'unkown';
     }
+}
+
+class LatLang {
+    lat!: number;
+    lon!: number;
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,7 +2,12 @@ import { View, Text } from 'react-native';
 import AppScreenContainer from '../components/AppScreenContainer';
 import AppBar from '../components/AppBar';
 import { styles } from '../utils/styles';
+import Geolocation from '@react-native-community/geolocation';
 
+Geolocation.getCurrentPosition(info => console.log(info));
+// TODO: success => zmien coordynaty na miejscowosc + pobierz dane na temat pogody 
+// TODO: failure => stan błędu (nie udało się pobrać lokalizacji) + przycisk odświez
+// TODO: failure pobierania pogody => stan błędu (nie udało się pobrać lokalizacji) + przycisk odświez
 
 const HomeScreen = () => {
     return (
